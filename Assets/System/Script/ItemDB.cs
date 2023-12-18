b1cfaf430b2366dc4f025a19e5c1c600707ab6fb
JangTong <jangtong0306@gmail.com>
2023-12-19 03:45:39 +0900

    DragonChef v0.3

---
+++ Assets/System/Script/ItemDB.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public static ItemDB instance;
+    private void Awake() {
+        instance = this;
+    }
+    
+    //public List<Items> itemDB = new List<Items>();
+    public List<TypeofItem> typeofitem = new List<TypeofItem>();
+    
+    public GameObject fieldItemPrefab;
+    public Vector3[] pos;
+    
+    private void Start() {
+        // for문을 이용해 아이템 여러개 만들기, pos[]는 위치 지정, Instantiate로 fieldItemPrefab clone하는 함수
+        for (int i = 1; i<11; i++) {
+            GameObject go = Instantiate(fieldItemPrefab, pos[i], Quaternion.identity);
+            go.GetComponent<FieldItems>().SetItem(typeofitem[1]);
+        }
+    }
+    
+}
